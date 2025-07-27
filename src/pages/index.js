a5d1d912f7e51773144f75d7042dabf1d71ed9eb
안상환 <vooo10@naver.com>
2025-07-27 19:53:24 +0900

    2025포트폴리오 리뉴얼(home 적용)

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,28 +4,88 @@ import { StaticImage } from "gatsby-plugin-image";
 import Seo from "../components/seo";
 const IndexPage = () => {
   return (
-    <Layout pageTitle="" pageClassName="sanghwan-portfolio--home">
+    <Layout
+      pageTitle={
+        <>
+          <p className="page-title__role">
+            <span className="stroke">Web UI</span>{" "}
+            <span className="stroke">Developer</span>
+          </p>
+
+          <p className="page-title__name">SANGHWAN AN</p>
+        </>
+      }
+      pageClassName="sanghwan-portfolio--home"
+    >
       {/* <div className="home-me-wrap">
         <span></span>
       </div> */}
-      <div className="home-intro-cont">
-        <div className="ash-character">
-          <div className="ash-character__inner">
-            <span className="ash-character__shining"></span>
+      <div className="home-cont">
+        <div className="ash-greeting">
+          <div className="ash-greeting__inner">
+            <div className="ash-hello">
+              <p>
+                안녕하세요, <br />
+                웹퍼블리셔 안상환 입니다.
+              </p>
+              <p>
+                정돈과 구조에 강하며, 프로젝트를 안정적으로 이끄는 힘이
+                있습니다.
+              </p>
+              <p>실용적인 감각과 꼼꼼함으로, 퍼블리싱의 완성도를 높입니다.</p>
+              <p>누구보다 조용히, 그러나 확실하게 팀에 기여하는 사람입니다.</p>
+              <p>
+                한 번 맺은 인연은 오래 지키고, 한 번 맡은 일은 끝까지 책임지는
+                성격입니다.
+              </p>
+              <p>
+                정리와 구조를 좋아하지만, 그 안에 따뜻한 감정을 담고자 합니다.
+              </p>
+              <p>사람과 일, 어느 쪽이든 진심으로 대하고 싶습니다.</p>
+            </div>
+            <ul className="ahs-skill-list">
+              <li>
+                <span className="skill-highlight">HTML5 / CSS3 / SCSS</span>
+              </li>
+              <li>
+                <span className="skill-highlight">
+                  웹표준 마크업 / 재사용성 중심 마크업
+                </span>
+              </li>
+              <li>
+                <span className="skill-highlight">
+                  JavaScript (UI 중심 스크립트 구현)
+                </span>
+              </li>
+              <li>
+                <span className="skill-highlight">
+                  반응형 웹 / 크로스 브라우징 / 웹 접근성
+                </span>
+              </li>
+              <li>
+                <span className="skill-highlight">
+                  Git을 통한 실무 협업이 가능합니다
+                </span>
+              </li>
+              <li>
+                <span className="skill-highlight">
+                  React 환경에서 UI 마크업과 스타일링에 익숙합니다.
+                </span>
+              </li>
+            </ul>
           </div>
         </div>
-        <div className="home-intro eng-text">
-          <div className="home-intro__inner">
-            <span>HTML5</span>
-            <br />
-            <span>CSS3/SCSS</span>
-            <br />
-            <span>JAVASCRIPT</span>
-            <br />
+
+        <div className="ash-character">
+          <div className="ash-character__inner">
+            <div className="ash-character__img-wrap">
+              <span className="ash-character__img">
+                <img src="/images/ansanghwan_character.png" alt="" />
+              </span>
+            </div>
           </div>
         </div>
       </div>
-      <span className="home-intro-bg"></span>
     </Layout>
   );
 };
